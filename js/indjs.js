bb4a16a554c60162832677e7ce8e8a6c0715c152
szdytom <33175397+szdytom@users.noreply.github.com>
2020-12-02 19:57:18 +0800

    Update indjs.js

--- js/indjs.js
+++ js/indjs.js
@@ -1,6 +1,6 @@
 function compileStr(code){
     var c=String.fromCharCode(code.charCodeAt(0)+code.length);  
-    for(var i=1;i<code.length;i++){        
+    for(var i=1;i<code.length;i+=1){        
         c+=String.fromCharCode(code.charCodeAt(i)+code.charCodeAt(i-1));  
     }     
     return escape(c);
